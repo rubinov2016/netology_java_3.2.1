cdaeca6b8565691f2c5e6b6ad14be6f715776d62
Ivan Rubinov <lrubinov@yandex.ru>
2021-06-13 18:50:35 +0300

    v2

--- src/main/java/ru/netology/domain/Post.java
+++ src/main/java/ru/netology/domain/Post.java
@@ -16,6 +16,12 @@ public class Post {
     int rating;             //внутренний рейтинг поста для отображения самых интересных
     int editVersion;        //номер для хранения промежуточных версий постов, которые потом были отредактирвоаны или удалены
 
+    //поля, которые я добавил
+    int friendsAccess;       //поле для хранения прав доступа        ~~= friends_only
+    int isPinned;            // = is_pinned
+    int isAds;               // = marked_as_ads
+    int geoID;               //Информация о геоположении
+
     public void setDateTime(long postID) {
     }
     public void getDateTime(long postID) {
@@ -54,3 +60,31 @@ public class Post {
     }
     
 }
+
+// какие поля у меня отсутствовали
+//created_by - ID of the administrator who posted the post (for communities only, returns for the requests with administrator's access token).
+//reply_post_id - Replied post's id.
+//friends_only - 1 if a post is marked "Friends only".
+//post_type Type of the post, can be: post, copy, reply, postpone, suggest.
+//post_source* Information about the means of posting (see Description of "post_source" field).
+//attachments Information about attachments to the post (photos, links, etc.), if any; see
+//can_pin Shows if current user can pin a post (1 – yes, 0 – no).
+//can_delete Shows if current user can delete the post (1 – yes, 0 – no).
+//can_edit Shows if current user can edit the post (1 – yes, 0 – no)
+//is_pinned Shows if the post is pinned (1 – yes, 0 – no).
+//marked_as_ads Information whether the post is marked as ads
+//is_favorite true, if object is added to favorites by current users.
+
+//класс comments
+//can_post* (integer, [0,1]) — shows if current user can comment the post. ("1 — yes, 0'' — no);
+//groups_can_post (integer, [0,1]) — information whether communities can comment this post.
+
+//класс   likes
+//user_likes* (integer, [0,1]) — Whether the user liked the post (0 — not liked, 1 — liked).
+//can_like* (integer, [0,1]) — Whether the user can like the post (0 — cannot, 1 — can)
+//can_publish* (integer, [0,1]) — Whether the user can repost (0 — cannot, 1 — can).
+
+//класс   reposts
+//user_reposted* (integer, [0,1]) — Whether the user reposted the post (0 — not reposted, 1 — reposted).
+
+//класс geo
